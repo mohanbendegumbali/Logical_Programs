dd952fa67f5a131cdf9a2ee27d71b2f1ff2ffc0a
mohanbendegumbali <mohan0311@hotmail.com>
2022-11-02 08:58:06 +0530

    [Mohan] Add: Folder Structure Created

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void Print( int num)
+        {
+            int firstnum = 0, secondnum = 1, thirdnum =0;
+            Console.WriteLine(firstnum + "\n" + secondnum);
+            for ( int i = 0; i < num; i++)
+            {
+                thirdnum = firstnum + secondnum;
+                firstnum=secondnum;
+                secondnum=thirdnum;
+                Console.WriteLine(thirdnum);
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿using LogicalPrograms;
+using System; 
+
+namespace Demo 
+{
+    class Program 
+    {
+        int a = 10;
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine(" Welcome");
+            FibonacciSeries fib = new FibonacciSeries();
+            fib.Print(5);
+        }
+    }
+}
