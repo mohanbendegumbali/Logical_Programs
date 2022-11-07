c2d5916c260d6763f73ede152aa81b3e86038335
mohanbendegumbali <mohan0311@hotmail.com>
2022-11-07 20:31:37 +0530

    [Mohan] Add: Day Of Week

---
+++ LogicalPrograms/Dayofweek.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Dayofweek2
+    {
+        public void Weekday()
+        {
+            Console.WriteLine("1.January" + "\n" + "2.February" + "\n" + "3.March" + "\n" + "4.April" + "\n" + "5.May" + "\n" + "6.June" + "\n" + "7.July" + "\n" + "8.August" + "\n" + "9.September" + "\n" + "10.October" + "\n" + "11.November" + "\n" + "12.December");
+            Console.WriteLine(" Enter the Month");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter the Year");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter the Day");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine(" Sunday ");
+                    break;
+                case 1:
+                    Console.WriteLine(" Monday ");
+                    break;
+                case 2:
+                    Console.WriteLine(" Tuesday ");
+                    break;
+                case 3:
+                    Console.WriteLine(" Wednesday ");
+                    break;
+                case 4:
+                    Console.WriteLine(" Thursday ");
+                    break;
+                case 5:
+                    Console.WriteLine(" Friday ");
+                    break;
+                case 6:
+                    Console.WriteLine(" Saturday ");
+                    break;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,25 +1,60 @@
 ﻿using LogicalPrograms;
-using System; 
+using System;
+using DayOfWeek = System.DayOfWeek;
 
-namespace Demo 
+namespace Demo
 {
-    class Program 
+    class Program
     {
         int a = 10;
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Logical Programs ");
-            //FibonacciSeries fib = new FibonacciSeries();
-            //fib.Print(5);
-            //PerfectNumber perfect = new PerfectNumber();
-            //perfect.check(28);
-            //PrimeNumber prime = new PrimeNumber();
-            //prime.check(5);
-            //ReverseNumber num = new ReverseNumber();
-            //num.reverse(345);
-            //CouponNumber.Generatecoupons(10);
-            StopWatch stop = new StopWatch();
-            stop.Start();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Fibonacci Series" + "\n" + "2.Perfect Number" + "\n" + "3.Prime Number" + "\n" + "4.Reverse Number" + "\n" + "5.Coupon Number" + "\n" + "6.Stop Watch" + "\n" + "7.Vending Machine" + "\n" + "8.Day of Week");
+                Console.WriteLine(" Enter the option ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fib = new FibonacciSeries();
+                        fib.Print(5);
+                        break;
+                    case 2:
+                        PerfectNumber perfect = new PerfectNumber();
+                        perfect.check(28);
+                        break;
+                    case 3:
+                        PrimeNumber prime = new PrimeNumber();
+                        prime.check(5);
+                        break;
+                    case 4:
+                        ReverseNumber num = new ReverseNumber();
+                        num.reverse(345);
+                        break;
+                    case 5:
+                        CouponNumber.Generatecoupons(10);
+                        break;
+                    case 6:
+                        StopWatch stop = new StopWatch();
+                        stop.Start();
+                        break;
+                    case 7:
+                        VendingMachine machine = new VendingMachine();
+                        machine.Notes(1500);
+                        machine.Print();
+                        break;
+                    case 8:
+                        //DayOfWeek day = new DayOfWeek();
+                        //day.Weekday();
+                        Dayofweek2 dayofweek2 = new Dayofweek2();
+                        dayofweek2.Weekday();
+                        break;
+                        
+
+                }
+            }
         }
     }
 }
